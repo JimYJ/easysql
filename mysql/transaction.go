5d8fed1e45667da60c6e60f38a3eb2adaf90e254
yangji <yangji@19licai.com>
2018-06-05 16:58:40 +0800

    isolation level for tx

--- mysql/transaction.go
+++ mysql/transaction.go
@@ -1,13 +1,37 @@
 package mysql
 
 import (
+	"context"
+	"database/sql"
 	"errors"
+	"time"
 )
 
-//TxBegin transaction begin
+//TxBegin transaction begin with default isolation level is dependent
 func (mdb *MysqlDB) TxBegin() error {
 	var err error
 	mdb.tx, err = mdb.dbConn.Begin()
+	printErrors(err)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// TxBeginWithIsol transaction begin with custom isolation level is dependent
+// LevelDefault 默认级别
+// LevelReadUncommitted 读未提交
+// LevelReadCommitted 读已提交
+// LevelWriteCommitted 写已提交
+// LevelRepeatableRead 可重复读
+// LevelSnapshot 可读快照
+// LevelSerializable 可串行化
+// LevelLinearizable 可线性化
+func (mdb *MysqlDB) TxBeginWithIsol(opts *sql.TxOptions) error {
+	var err error
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Millisecond)
+	defer cancel()
+	mdb.tx, err = mdb.dbConn.BeginTx(ctx, opts)
 	printErrors(err)
 	if err != nil {
 		return err
